209448c33a46a2f4cde44398e78826a3a9a850d3
Carlo Costa <carlo.costa92@gmail.com>
2025-03-26 16:07:17 +0100

    feat: create new file for elements

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//import data.js
+const post = require("./data.js");
+
 //configurazione asset
 app.use(express.static('public'));
 
@@ -20,44 +23,7 @@ app.get("/" , (req,res) => {
 
 //definizione rotta /bacheca
 app.get("/bacheca" , (req,res) => {
-
-    const post=[
-
-        {
-            titolo: "ciambellone",
-            contenuto: "ciambella con zucchero",
-            immagine: "images/ciambellone.jpeg",
-            tags: ["piatto" , "zucchero"]
-        },
-        {
-            titolo: "craker",
-            contenuto: "tagliere con crakers",
-            immagine: "images/cracker_barbabietola.jpeg",
-            tags: ["craker" , "barbabietola"]
-        },
-        {
-            titolo: "panefritto",
-            contenuto: "piatto con pan fritto",
-            immagine: "images/pane_fritto_dolce.jpeg",
-            tags: ["piatto" , "panfritto"]
-        },
-        {
-            titolo: "pasta",
-            contenuto: "piatto di pasta",
-            immagine: "images/pasta_barbabietola.jpeg",
-            tags: ["piatto" , "pasta"]
-        },
-        {
-            titolo: "torta",
-            contenuto: "torta su portatorte",
-            immagine: "images/torta_paesana.jpeg",
-            tags: ["portatorta" , "torta"]
-        },
-
-    ]
-
     res.json(post);
-
 });
 
 
---
+++ data.js
@@ -0,0 +1,36 @@
+const post=[
+
+    {
+        titolo: "ciambellone",
+        contenuto: "ciambella con zucchero",
+        immagine: "./images/ciambellone.jpeg",
+        tags: ["piatto" , "zucchero"]
+    },
+    {
+        titolo: "craker",
+        contenuto: "tagliere con crakers",
+        immagine: "./images/cracker_barbabietola.jpeg",
+        tags: ["craker" , "barbabietola"]
+    },
+    {
+        titolo: "panefritto",
+        contenuto: "piatto con pan fritto",
+        immagine: "./images/pane_fritto_dolce.jpeg",
+        tags: ["piatto" , "panfritto"]
+    },
+    {
+        titolo: "pasta",
+        contenuto: "piatto di pasta",
+        immagine: "./images/pasta_barbabietola.jpeg",
+        tags: ["piatto" , "pasta"]
+    },
+    {
+        titolo: "torta",
+        contenuto: "torta su portatorte",
+        immagine: "./images/torta_paesana.jpeg",
+        tags: ["portatorta" , "torta"]
+    },
+
+];
+
+module.exports = post;
